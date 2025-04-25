a0902943e0577bd975cd7ad9a282c3ad259bfcc8
willdomkahari <developer.wadu@gmail.com>
2025-04-25 16:04:27 +0200

    FEAT: add find user by name

--- user/src/main/java/com/waduclay/homefinder/user/BaseUserRepositoryPort.java
+++ user/src/main/java/com/waduclay/homefinder/user/BaseUserRepositoryPort.java
@@ -1,10 +1,13 @@
 package com.waduclay.homefinder.user;
 
 
+import java.util.Optional;
+
 /**
  * @author <a href="mailto:developer.wadu@gmail.com">Willdom Kahari</a>
  */
 public interface BaseUserRepositoryPort {
     boolean existsByRole(Role role);
+    Optional<BaseUser> findByUsername(String username);
     void save(BaseUser user);
 }
