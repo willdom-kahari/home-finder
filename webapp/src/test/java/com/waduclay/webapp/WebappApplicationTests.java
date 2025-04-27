b73fa4f5cdb328441d44699178119297c4f10656
willdomkahari <developer.wadu@gmail.com>
2025-04-27 12:53:42 +0200

    INIT: web app

---
+++ webapp/src/test/java/com/waduclay/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waduclay.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
