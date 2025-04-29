1800eda87c191d15f468d4db1fc9667c840fbce9
willdomkahari <developer.wadu@gmail.com>
2025-04-29 21:38:46 +0200

    FEAT: define password generation rules

---
+++ application/src/main/java/com/waduclay/application/config/RandomGenerator.java
@@ -0,0 +1,25 @@
+package com.waduclay.application.config;
+
+
+
+import io.github.willdomkahari.generator.CharacterGenerator;
+import io.github.willdomkahari.generator.Characters;
+import io.github.willdomkahari.generator.GeneratorRule;
+
+/**
+ * @author <a href="mailto:developer.wadu@gmail.com">Willdom Kahari</a>
+ */
+public final class RandomGenerator {
+    private static final CharacterGenerator characterGenerator = new CharacterGenerator();
+
+    private RandomGenerator() {
+    }
+    public static String characters(int length) {
+        return characterGenerator.generateCharacters(
+                length, new GeneratorRule(Characters.CAPITAL),
+                new GeneratorRule(Characters.NUMERIC),
+                new GeneratorRule(Characters.SPECIAL),
+                new GeneratorRule(Characters.SMALL)
+        );
+    }
+}
