3a9ea6a3a6a59bc8e797778415056e4f3c952cb6
willdomkahari <developer.wadu@gmail.com>
2025-04-29 21:34:29 +0200

    FEAT: event publisher adapter

---
+++ application/src/main/java/com/waduclay/application/config/ApplicationEventPublisherAdapter.java
@@ -0,0 +1,26 @@
+package com.waduclay.application.config;
+
+
+import com.waduclay.homefinder.ports.EventPublisher;
+import com.waduclay.homefinder.shared.DomainEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author <a href="mailto:developer.wadu@gmail.com">Willdom Kahari</a>
+ */
+@Component
+@RequiredArgsConstructor
+public class ApplicationEventPublisherAdapter implements EventPublisher {
+    private final ApplicationEventPublisher applicationEventPublisher;
+    @Override
+    public void publish(DomainEvent event) {
+        applicationEventPublisher.publishEvent(event);
+    }
+
+    @Override
+    public void publishAll(Iterable<DomainEvent> events) {
+        events.forEach(this::publish);
+    }
+}
