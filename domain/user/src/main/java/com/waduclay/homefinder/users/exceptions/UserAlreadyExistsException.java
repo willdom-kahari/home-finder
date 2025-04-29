58862929480d48823318557cb72fed58081e23c0
willdomkahari <developer.wadu@gmail.com>
2025-04-29 21:30:07 +0200

    FEAT: domain exceptions

---
+++ domain/user/src/main/java/com/waduclay/homefinder/users/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,30 @@
+package com.waduclay.homefinder.users.exceptions;
+
+/**
+ * Exception thrown when attempting to create a user with a username that already exists.
+ * This exception is used to indicate that a user with the specified username already exists
+ * in the system, and therefore a new user with the same username cannot be created.
+ * 
+ * @author <a href="mailto:developer.wadu@gmail.com">Willdom Kahari</a>
+ */
+public class UserAlreadyExistsException extends DomainException {
+    
+    /**
+     * Creates a new UserAlreadyExistsException with the specified message.
+     * 
+     * @param message the detail message
+     */
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+    
+    /**
+     * Creates a new UserAlreadyExistsException with the specified message and cause.
+     * 
+     * @param message the detail message
+     * @param cause the cause of the exception
+     */
+    public UserAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ domain/user/src/main/java/com/waduclay/homefinder/users/exceptions/ValidationException.java
@@ -0,0 +1,30 @@
+package com.waduclay.homefinder.users.exceptions;
+
+/**
+ * Exception thrown when validation of input data fails.
+ * This exception is used to indicate that the input data provided to a domain operation
+ * does not meet the required validation rules.
+ * 
+ * @author <a href="mailto:developer.wadu@gmail.com">Willdom Kahari</a>
+ */
+public class ValidationException extends DomainException {
+    
+    /**
+     * Creates a new validation exception with the specified message.
+     * 
+     * @param message the detail message
+     */
+    public ValidationException(String message) {
+        super(message);
+    }
+    
+    /**
+     * Creates a new validation exception with the specified message and cause.
+     * 
+     * @param message the detail message
+     * @param cause the cause of the exception
+     */
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
