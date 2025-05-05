74615ae9c7f873424f3a20f8d0eb0559f8224282
willdomkahari <developer.wadu@gmail.com>
2025-05-05 19:58:24 +0200

    FEAT: define flash notifications

---
+++ application/src/main/java/com/waduclay/application/notification/NotificationCategory.java
@@ -0,0 +1,9 @@
+package com.waduclay.application.notification;
+
+
+/**
+ * @author <a href="mailto:developer.wadu@gmail.com">Willdom Kahari</a>
+ */
+public enum NotificationCategory {
+    ERROR, INFO, WARN, SUCCESS
+}
